987fa2e733f94af9a21cd6fc0fd4ceb3362b35c7
NritiNy <nritiny@gmail.com>
2023-12-01 17:54:36 +0100

    Added Solution for AoC 2023
    Solved Day 01

---
+++ C#/AdventOfCode2023/Day01.cs
@@ -0,0 +1,83 @@
+﻿using AdventOfCodeLib;
+
+public static class Day01
+{
+
+    public static Puzzle.Solution Part1 = (input) =>
+    {
+        var sum = 0;
+        foreach (var line in input)
+        {
+            sum += int.Parse($"{line.First((c) => char.IsDigit(c))}{line.Last((c) => char.IsDigit(c))}");
+        }
+
+        return sum.ToString();
+    };
+
+    public static Puzzle.Solution Part2 = (input) =>
+    {
+        var digits = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        var sum = 0;
+        foreach (var line in input)
+        {
+            string d1 = "0";
+            string d2 = "0";
+
+            var counter = 1;
+            var index = line.Length + 1;
+            foreach (var digit in digits)
+            {
+                var idx = line.IndexOf(digit);
+                if (0 <= idx && idx < index)
+                {
+                    d1 = counter.ToString();
+                    index = idx;
+                }
+                counter++;
+            }
+
+            if (d1 == "0")
+            {
+                d1 = line.First((c) => char.IsDigit(c)).ToString();
+            }
+            else if (line.Any((c) => char.IsDigit(c)))
+            {
+                var d = line.First((c) => char.IsDigit(c));
+                if (line.IndexOf(d) < index)
+                {
+                    d1 = d.ToString();
+                }
+            }
+
+            counter = 1;
+            index = 0;
+            foreach (var digit in digits)
+            {
+                var idx = line.LastIndexOf(digit);
+                if (0 < idx && idx > index)
+                {
+                    d2 = counter.ToString();
+                    index = idx;
+                }
+                counter++;
+            }
+
+            if (d2 == "0")
+            {
+                d2 = line.Last((c) => char.IsDigit(c)).ToString();
+            }
+            else if (line.Any((c) => char.IsDigit(c)))
+            {
+                var d = line.Last((c) => char.IsDigit(c));
+                if (line.LastIndexOf(d) > index)
+                {
+                    d2 = d.ToString();
+                }
+            }
+
+            sum += int.Parse($"{d1}{d2}");
+        }
+
+        return sum.ToString();
+    };
+}
---
+++ C#/AdventOfCode2023/Program.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCodeLib;
+using System.Reflection;
+
+
+var year = DateTime.Now.Year;
+var day = DateTime.Now.Day;
+
+var puzzle = new Puzzle(year, day);
+var input = puzzle.GetInputLines();
+
+
+Type? Day = Assembly.GetExecutingAssembly().GetType($"Day{day:00}");
+if (null == Day)
+{
+    Console.WriteLine($"No solutions available yet for Day {day:00}");
+    return;
+}
+Console.WriteLine($"Day{day:00}");
+
+
+FieldInfo? Part1Field = Day.GetField("Part1", BindingFlags.Static | BindingFlags.Public);
+if (null == Part1Field || null == Part1Field.GetValue(Day))
+{
+    Console.WriteLine($"No solution available yet for Day {day:00} Part1");
+    return;
+}
+
+if (Part1Field.GetValue(Day) is Puzzle.Solution Part1)
+{
+    var solution = Part1(input);
+    Console.WriteLine($"Part1: {puzzle.Submit1(solution)} ({solution})");
+} 
+else
+{
+    Console.WriteLine($"No solution available yet for Day {day:00} Part1");
+    return;
+}
+
+
+FieldInfo? Part2Field = Day.GetField("Part2", BindingFlags.Static | BindingFlags.Public);
+if (null == Part2Field || null == Part2Field.GetValue(Day))
+{
+    Console.WriteLine($"No solution available yet for Day {day:00} Part2");
+    return;
+}
+
+if (Part2Field.GetValue(Day) is Puzzle.Solution Part2)
+{
+    var solution = Part2(input);
+    Console.WriteLine($"Part2: {puzzle.Submit2(solution)} ({solution})");
+}
+else
+{
+    Console.WriteLine($"No solution available yet for Day {day:00} Part1");
+    return;
+}
