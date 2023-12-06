31933f62d61ad99d7214e34f8d3ee047766a0cfa
NritiNy <nritiny@gmail.com>
2023-12-06 08:08:44 +0100

    Solved Day 06

---
+++ C#/AdventOfCode2023/Day06.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCodeLib;
+
+public class Day06
+{
+    public static Puzzle.Solution Part1 = (input) =>
+    {
+        var times = input[0].Split(": ")[1].Split("  ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(t => int.Parse(t)).ToList();
+        var distances = input[1].Split(": ")[1].Split("  ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(t => int.Parse(t)).ToList();
+
+        var counts = new List<int>();
+        for(int i = 0; i < times.Count; ++i)
+        {
+            var time = times[i];
+            var distance = distances[i];
+
+            var count = 0;
+            for  (int t = 0; t < time; ++t)
+            {
+                if (distance < (t * (time - t))) count++;
+            }
+            counts.Add(count);
+        }
+        return counts.Aggregate((f, s) => f * s).ToString();
+    };
+
+    public static Puzzle.Solution Part2 = (input) =>
+    {
+        var time = long.Parse(string.Join("", input[0].Split(": ")[1].Split("  ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)));
+        var distance = long.Parse(string.Join("", input[1].Split(": ")[1].Split("  ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)));
+
+        var count = 0;
+        for (long t = 0; t < time; ++t)
+        {
+            if (distance < (t * (time - t))) count++;
+        }
+
+        return count.ToString();
+    };
+}
